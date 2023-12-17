252e623c8751f0740f2b151bcc5b98410e1b4fef
JuanDeDiosGarcia <juandediosgasal@gmail.com>
2023-12-17 18:41:53 +0100

    Problems on project

--- Valorant Agent/android/app/src/main/kotlin/com/example/api_data_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.api_data_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Valorant Agent/lib/models/user.dart
+++
@@ -1,9 +0,0 @@
-class User {
-  String firstName, description;
-  String avatarUrl;
-
-  User.fromJson(Map<String, dynamic> json)
-      : firstName = json["displayName"],
-        description = json["description"],
-        avatarUrl = json["displayIcon"];
-}
